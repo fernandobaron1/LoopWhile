5dafc2a9f61b4da0f11dd292bb0bf4f308fe2cde
Fernando Baron <fernandobaron1@hotmail.com>
2019-08-04 20:54:05 -0300

    Modelo 1

---
+++ LoopWhile/src/loopwhile/LoopWhile.java
@@ -0,0 +1,21 @@
+
+package loopwhile;
+
+
+public class LoopWhile {
+
+    
+    public static void main(String[] args) {
+        
+         int contador = 1;
+     
+     while (contador <= 10) {
+         
+         if (contador % 2 ==0){
+         System.out.println ("Contador:"+contador);
+         }
+         contador++;
+     }
+    }
+    
+}
